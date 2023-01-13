894c9211fa2cbeb6f4eb1348d8ab1512a3b54339
Sergey Tishchenko <seti.volch@gmail.com>
2023-01-13 19:35:33 +0700

    growl and main

--- src/Zombie.java
+++ src/Zombie.java
@@ -1,12 +1,15 @@
 public class Zombie extends Monster {
     public Zombie (String name) {
-        super(name, 5);
+        super(name + "The Zombie", 5);
     }
 
     @Override
     public void growl() {
-        System.out.print("Raaaauuughhhh ");
+        System.out.print("Raaaauuughhhh");
         super.growl();
+    }
+    public void attack() {
+        super.attack();
     }
 
     public static void main(String[] args) {
